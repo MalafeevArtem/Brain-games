636bd199c8bdfa0db355c7b2b992ed09106fedd7
Malafeev Artem <m.a.yevgenyevich@gmail.com>
2019-10-16 19:36:48 +0300

    change the name of the variable (brain-even.js)

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -5,9 +5,9 @@ import play from '..';
 const REGULATIONS = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const gameData = () => {
-  const number = randomNumber(0, 100);
-  const isEven = (num) => num % 2 === 0;
-  const trueAnswer = isEven(number) ? 'yes' : 'no';
+  const question = randomNumber(0, 100);
+  const isEven = (number) => number % 2 === 0;
+  const trueAnswer = isEven(question) ? 'yes' : 'no';
   console.log(`Question: ${number}`);
   const askAnswerUser = readlineSync.question('Your answer: ');
 
