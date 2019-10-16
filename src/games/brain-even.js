cd1acb6ca644b3984a34448d4467824fd1cf27a7
Malafeev Artem <m.a.yevgenyevich@gmail.com>
2019-10-16 20:19:36 +0300

    make a function definition (brain-even)

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,9 +4,10 @@ import play from '..';
 
 const REGULATIONS = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = number => number % 2 === 0;
+
 const gameData = () => {
   const question = randomNumber(0, 100);
-  const isEven = (number) => number % 2 === 0;
   const trueAnswer = isEven(question) ? 'yes' : 'no';
   const data = [trueAnswer, question];
 
